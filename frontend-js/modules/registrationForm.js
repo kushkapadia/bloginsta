72487fb600caaf32551c016872bd4d1746cb41ad
Kush <kush2004kapadia@gmail.com>
2022-02-01 19:49:25 +0530

    Password length

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -96,8 +96,8 @@ export default class RegistrationForm {
   }
 
   passwordAfterDelay() {
-    if (this.password.value.length < 12) {
-      this.showValidationError(this.password, "Password must be at least 12 characters.")
+    if (this.password.value.length < 7) {
+      this.showValidationError(this.password, "Password must be at least 7 characters.")
     }
   }
 
